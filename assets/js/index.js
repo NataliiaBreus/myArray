cd48f74d23eeea1f4e03f43b5fae8fd92841dde0
NataliiaBreus <breusnataly@gmail.com>
2021-04-06 10:25:42 +0300

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,161 +1,64 @@
-//alert("Hello world");
-
-//alert("Buy world!");
-/*
-console.log("Hello world");
-console.log("test");
-console.log("10");
-console.log(10);
-console.log(true);
-console.log(false);
-console.log(null);
-console.log(undefined)
-const helloStr = 'Hello world';
-console.log(helloStr)
-let boolVariable = true;
-console.log(boolVariable, typeof boolVariable)
-const test = undefined;
-const variable = null;
-
-console.log(test);
-console.log(variable);
-let data = 10;
-if ('test') {
-    console.log("success")
+'use strict';
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(Math.random() * (max-min+1)) + min;
 }
-*/
-/*
-let a = 2;
-let b = 3;
-console.log(a*b);
-let c = 5;
-let d = 7;
-console.log(c/d);
-let e = 4;
-let f = 6;
-console.log(e+f);
-let test = 11;
-let bol = true;
-let str = "java script";
-let text = "100";
-console.log(test, bol, str, text);
 
-let num = 1;
-num += 11;
-num -= 11;
-num *= 11;
-num /=11;
-num +=1;
-num -= 1;
-console.log(num);
-*/
-/*
-const userInput = prompt();
-alert(userInput);
-
-const myFirstFunc = function(){
-    console.log("1");
+const numsArr = [];
+for (let i = 0; i<10; i++){
+  numsArr.push(getRandomIntInclusive(100, 200));
 }
-myFirstFunc();
-myFirstFunc();
-myFirstFunc();
-*/
 
-// Task 1
-const userInput1 = prompt("Enter number 1:");
-const userInput2 = prompt ("Enter number 2:");
-
-const maxNumber = function(val1, val2){
-    return val1 > val2? val1 : val2;
-
+function User(firstName, lastName, email, age, isMale, isSubscribed = false){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.age = age;
+  this.isMale = isMale;
+  this.isSubscribed = isSubscribed;
 }
-const value = maxNumber(userInput1, userInput2);
-console.log(value);
+const NATIONALITIES = [
+  'uk',
+  'us',
+  'bg',
+  'ru',
+  'uk',
+  'fr',
+]
 
-// Task 2
-
-const minNumber = function(val1, val2){
-    return val1 < val2? val1 : val2;
-
+User.prototype = new UserProto();
+function UserProto(){
+this.getFullName = function(){
+  return `${this.firstName} ${this.lastName}`;
 }
-const number = minNumber(userInput1, userInput2);
-console.log(number);
-
-// Task 3
- const isOdd = function(num){
-     if(num % 2 === 0){
-         return("чётное");
-     } 
-         return (num, "нечётное");
+}
+ function getRandomUsers (amount){
+   const usersArray = [];
+   for (let i = 0; i < amount; i++){
+     usersArray.push(
+       new User(
+         `Name${i}`, //first name
+         `Surname${i}`, // last name
+         `user${i}@gmail.com`, //email
+         getRandomIntInclusive(10, 50), // age
+         getRandomNationality(),
+         Math.random() < 0.5, // isMale
+         Math.random() <0.5 // isSubscribed
+       )
+     );
+   }
+   return usersArray;
  }
- console.log(isOdd(userInput1));
+
+ function getRandomNationality(){
+   return NATIONALITIES [getRandomIntInclusive(0,NATIONALITIES.length - 1)];
+ }
+
+ const userList = getRandomUsers(100);
+
+ function getSubscribedAdultWoman(arr){
+ return arr.filter(function(user){
+  return user.age >= 18 && !user.isMale && user.isSubscribed;
+});
+ }
  
- // Task 4 (решение квадратного уравнения)
- const a = + prompt ("Enter a:");
- const b = + prompt ("Enter b:");
- const c = + prompt ("Enter c:");
-
- const quadratic = function (a,b,c){
-     const D = b**2 - 4 * a * c;
-     if (D<0){
-         console.log ("D<0");
-         return null;
-     }
-     if (D === 0){
-         const x = - b / (2 * a);
-         return [x];
-     }
-     const x1 = (- b + D** (1 / 2)) / (2 * a);
-     const x2 = (- b - D** (1 / 2)) / (2 * a);
-     return [x1, x2];
- }
- console.log(quadratic (a, b, c));
-
- // Task 5
-
- const day = +prompt("Введите число от 1 до 31:");
- switch (day){
-    case 1:
-    case 2:
-    case 3: 
-    case 4:
-    case 5: 
-    case 6:
-    case 7:
-    case 8: 
-    case 9:
-    case 10: {
-        console.log("первая декада");
-        break;
-    }
-    case 11:
-    case 12:
-    case 13: 
-    case 14:
-    case 15: 
-    case 16:
-    case 17:
-    case 18: 
-    case 19:
-    case 20: {
-console.log("вторая декада");
-break;
-    }
-    case 21:
-        case 22:
-        case 23: 
-        case 24:
-        case 25: 
-        case 26:
-        case 27:
-        case 28: 
-        case 29:
-        case 30:
-        case 31: {
-    console.log("третья декада");
-    break;
-        }
-        default:{
-            console.log("Число не из списка")
-        }
- }
